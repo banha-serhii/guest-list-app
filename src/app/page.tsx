ae557f871934e158ed4f146aea3af2dc5ac9f891
sergii <serhij.banha@gmail.com>
2024-10-19 11:24:39 +0300

    clear code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import AddGuestForm from './components/AddGuestForm';
 import GuestList from './components/GuestList';
 import { useLocalStorageGuests } from './hooks/useLocalStorageGuests';
 import ThemeToggle from './components/ThemeToggle';
-import LanguageSwitcher from "@/app/components/LanguageSwitcher";
 
 const Home: React.FC = () => {
     const { guests, addGuest, togglePlusOne, editGuest, deleteGuest } = useLocalStorageGuests();
@@ -14,7 +13,6 @@ const Home: React.FC = () => {
         <div className="container mx-auto p-4 min-h-screen">
             <div className="flex justify-between items-center mb-6">
                 <h1 className="text-3xl font-bold dark:text-white">Birthday Guest List</h1>
-                <LanguageSwitcher />
                 <ThemeToggle />
             </div>
             <AddGuestForm onAdd={addGuest} />
